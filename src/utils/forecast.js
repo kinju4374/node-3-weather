88f2570ca9cd5cdbc6425de9db3f2793ce22b6a5
Kinjan Bhavsar <kinju4374@gmail.com>
2021-05-22 18:43:58 +0530

    Added humidity information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, lng, callback) => {
           callback('Unable to find location');
       } 
       else {
-          callback(undefined, body.current.weather_descriptions[0] +'. It is currenlty ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.');
+          callback(undefined, body.current.weather_descriptions[0] +'. It is currenlty ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' +body.current.humidity);
       }
   })
 }
